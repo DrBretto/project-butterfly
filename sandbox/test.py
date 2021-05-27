485665fb979fcd31da70d4283f618457b3067300
Brett Westerlund <drbretto82@gmail.com>
2021-05-27 14:42:51 -0400

    experimenting with cnn

--- sandbox/test.py
+++ sandbox/test.py
@@ -41,3 +41,54 @@ probability_model = tf.keras.Sequential([model,
 predictions = probability_model.predict(test_images)
 
 print("prediction:", np.argmax(predictions[0]))
+
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+  
+  # Grab an image from the test dataset.
+img = test_images[1]
+
+print(img.shape)
+
+# Add the image to a batch where it's the only member.
+img = (np.expand_dims(img,0))
+
+print(img.shape)
+
+predictions_single = probability_model.predict(img)
+
+print(predictions_single)
+
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+
+print(np.argmax(predictions_single[0]))
+
